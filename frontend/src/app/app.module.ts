2b466365f39ff8176a1459e0f48c532e345b5350
akaMiike <mike.nss@alu.ufc.br>
2022-12-25 12:54:44 -0300

    Added footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
