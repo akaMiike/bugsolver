100edb62d1272c647c52ad1f7c022a824d4cc096
akaMiike <mike.nss@alu.ufc.br>
2023-01-27 00:45:07 -0300

    fix: queries to search bugs by categories/title

--- backend/src/main/java/com/bugsolver/repository/BugRepository.java
+++ backend/src/main/java/com/bugsolver/repository/BugRepository.java
@@ -17,19 +17,15 @@ public interface BugRepository extends JpaRepository<Bug, Long> {
     @Query(value = "SELECT b.* FROM Bugs as b JOIN Users as u ON b.user_id = u.id AND u.id = :idUser", nativeQuery = true)
     Page<Bug> findBugsByUserId(Pageable pageable, @Param("idUser") Long idUser);
 
-    @Query(value = "SELECT b.* FROM Bugs as b " +
-            "JOIN bug_category as bc ON bc.bug_id = b.id " +
-            "JOIN Category as c ON c.id = bc.category_id and c.name IN :categories", nativeQuery = true)
+    @Query(value = "SELECT b FROM Bug b JOIN b.categories c WHERE c.name IN :categories",
+            countQuery = "SELECT count(b) FROM Bug b LEFT JOIN b.categories c WHERE c.name IN :categories"
+    )
     Page<Bug> findBugsByCategories(Pageable pageable, @Param("categories") Set<String> categories);
 
-    @Query(value = "SELECT b FROM Bug b JOIN FETCH b.categories c " +
+    @Query(value = "SELECT b FROM Bug b JOIN b.categories c " +
             "WHERE UPPER(b.title) LIKE UPPER(CONCAT('%',:title,'%')) and c.name IN :categories",
             countQuery = "SELECT count(b) FROM Bug b JOIN b.categories c " +
             "WHERE UPPER(b.title) LIKE UPPER(CONCAT('%',:title,'%')) and c.name IN :categories"
     )
     Page<Bug> findBugsByCategoriesAndTitle(Pageable pageable, @Param("categories") Set<String> categories, @Param("title") String title);
-
-    @Query(value = "SELECT b from Bug b LEFT JOIN FETCH b.categories",
-            countQuery = "SELECT count(b) from Bug b")
-    Page<Bug> findAll(Pageable pageable);
 }
