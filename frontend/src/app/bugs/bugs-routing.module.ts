d549843ffeab875da10c8874515cf9f734571c5f
akaMiike <mike.nss@alu.ufc.br>
2023-01-30 23:41:12 -0300

    feat: update bug component w/ validations

--- frontend/src/app/bugs/bugs-routing.module.ts
+++ frontend/src/app/bugs/bugs-routing.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, UrlSegment } from '@angular/router';
 import { BugsComponent } from './components/bugs/bugs.component';
 import { BugDetailsComponent } from './components/bug-details/bug-details.component';
 import { NewBugComponent } from './components/new-bug/new-bug.component';
+import { UpdateBugComponent } from './components/update-bug/update-bug.component';
+
+export function bugDetailsMatcher(url: UrlSegment[]){
+  if(url.length == 1 && url[0].path.match(/^\d+$/)){
+    return {consumed: url.slice(0,1), posParams: {id: new UrlSegment(url[0].path, {})}}
+   }else return null
+}
+export function updateBugMatcher(url: UrlSegment[]){
+  if(url.length > 1 && url[0].path.match(/^\d+$/) && url[1].path === 'update'){
+    
+    const posParams: { [key: string]: UrlSegment } = {};
+    posParams['id'] = url[0];
+    posParams['update'] = url[1];
+    
+    return {consumed: url, posParams: posParams}
+   } else return null
+}
 
 const routes: Routes = [
   {path: '', component: BugsComponent, pathMatch: 'full'},
   {path: 'new', component: NewBugComponent, pathMatch: 'full'},
-  {path: ':id', component: BugDetailsComponent, pathMatch: 'full'},
+  {matcher: bugDetailsMatcher, component: BugDetailsComponent},
+  {matcher: updateBugMatcher, component: UpdateBugComponent},
   {path: '**', redirectTo: 'bugs'}
 ];
 
